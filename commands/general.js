88f78ac41c888b4b195bead871cd3219e999a75a
Y2003Er <yusuphhanigomba7@gmail.com>
2026-06-07 22:20:04 +0300

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -1,9 +1,8 @@
 // commands/general.js
-module.exports = {
-    name: 'ping',
-    description: 'Check if bot is alive',
-    async execute(sock, msg, args) {
-        const from = msg.key.remoteJid;
-        await sock.sendMessage(from, { text: '🏓 Pong! Bot iko hai.' });
-    }
-};
+export const name = 'ping';
+export const description = 'Check if bot is alive';
+
+export async function execute(sock, msg, args) {
+    const from = msg.key.remoteJid;
+    await sock.sendMessage(from, { text: '🏓 Pong! Bot iko hai.' });
+}
